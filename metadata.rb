3b772297cba570685e598edfff484b84b5443925
Justin Alan Ryan <juryan@ziprealty.com>
2015-12-09 10:52:43 -0800

    initial berks cookbook gen.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'zip_mesos'
+maintainer       'Justin Alan Ryan (ZipRealty / Realogy)'
+maintainer_email 'juryan@ziprealty.com'
+license          'Apache 2.0'
+description      'Installs/Configures zip_mesos'
+long_description 'Installs/Configures zip_mesos'
+version          '0.1.0'
