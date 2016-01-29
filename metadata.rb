3101666e8f2df5714eb111dd0161e9da11a3b837
Justin Alan Ryan <juryan@ziprealty.com>
2016-01-29 12:26:10 -0800

    install docker

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'juryan@ziprealty.com'
 license          'Apache 2.0'
 description      'Installs/Configures zip_mesos'
 long_description 'Installs/Configures zip_mesos'
-version          '0.2.2'
+version          '0.2.3'
 
 depends 'mesos'
 depends 'zookeeper'
 depends 'marathon'
+depends 'docker', '~> 2.0'
 
--- recipes/manager.rb
+++ recipes/manager.rb
@@ -21,6 +21,10 @@ include_recipe 'mesos::master'
 include_recipe 'marathon::install'
 include_recipe 'marathon::service'
 
+docker_service 'default' do
+  action [:create, :start]
+end
+
 hdfs_mesos_version = node[:zip_mesos][:hdfs_mesos][:version]
 hdfs_mesos_filename = "hdfs-mesos-#{hdfs_mesos_version}.tar.gz"
 hdfs_mesos_path = node[:zip_mesos][:hdfs_mesos][:path]
