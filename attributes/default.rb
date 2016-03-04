588f43ab919fe6be2cda9e50f55f5b2a7d4f44cd
Justin Alan Ryan <juryan@ziprealty.com>
2016-03-04 14:56:41 -0800

    zk server list default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default[:zip_mesos][:hdfs_mesos][:version] = '0.1.6'
 default[:zip_mesos][:hdfs_mesos][:path] = '/opt/hdfs-mesos/'
 default['mesos']['master']['flags']['quorum'] = 1
 default['chronos']['jar_source'] = 'https://github.com/mesos/chronos/archive/2.4.0.tar.gz'
+default['chronos']['zookeeper_server_list'] = [ 'zk01', 'zk02', 'zk03' ]
 
