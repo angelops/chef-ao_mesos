631b3dbadd2790acb3cbb4073a598097125d6d46
Justin Alan Ryan <juryan@ziprealty.com>
2016-06-15 12:58:28 -0700

    update mesos version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default[:zip_mesos][:hdfs_mesos][:path] = '/opt/hdfs-mesos/'
 default['mesos']['master']['flags']['quorum'] = 1
 force_default['mesos']['slave']['flags']['work_dir'] = '/var/mesos'
 force_default['mesos']['master']['flags']['work_dir'] = '/var/mesos'
-default['mesos']['version'] = '0.27.1'
+default['mesos']['version'] = '0.28.2'
 default['chronos']['jar_source'] = 'https://github.com/mesos/chronos/archive/2.4.0.tar.gz'
 default['chronos']['zookeeper_server_list'] = [ 'zk01', 'zk02', 'zk03' ]
 
